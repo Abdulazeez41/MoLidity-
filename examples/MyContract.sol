3d85c020d0304aeeab310d02b77412b3ba10331b
Abdulazeez41 <abdulazezsalihu41@gmail.com>
2025-05-20 06:34:29 +0100

    docs: add placeholder to initialize docs directory

---
+++ examples/MyContract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    uint256 private value;
+
+    function storeValue(uint256 x) public {
+        value = x;
+    }
+
+    function getValue() public view returns (uint256) {
+        return value;
+    }
+}
